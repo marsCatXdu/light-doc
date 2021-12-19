87dfa9ec49aa57b3ab35bcfd14663f023e302546
lijingwei <lijingwei@uniontech.com>
2021-12-19 19:13:40 +0800

    Make document as a child of home. Since it is a drawer in home page

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,15 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: ':filename',
+        name: 'document',
+        component: () => import('../views/Document.vue'),
+        props: true
+      }
+    ]
   },
   {
     path: '/about',
@@ -14,11 +22,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/document/:filename',
-    name: 'Document',
-    component: () => import('../views/Document.vue')
   }
 ]
 
