61ba2b5724fc61f368546996d385ffdbf541c28b
lijingwei <lijingwei@uniontech.com>
2021-12-19 16:45:00 +0800

    change global align to left-align

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <!--
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+  -->
   <router-view/>
 </template>
 
@@ -11,8 +13,9 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  padding: 0px;
+  margin: 0px;
 }
 
 #nav {
