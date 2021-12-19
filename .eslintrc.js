bdb43ea01c9e9d507d864c0b48dbeb68a7785fe3
lijingwei <lijingwei@uniontech.com>
2021-12-19 16:42:01 +0800

    make lint config fits my taste

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0,
+    'semi': [0, 'never'],
+    'space-before-function-paren': [2, "never"],
+    'no-multi-spaces': 0,
+    'prefer-const': 0,
+    'quotes': 0,
   }
 }
